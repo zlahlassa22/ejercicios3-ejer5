440e1a20116fbdd0910c6b62185a77a973dba498
zakaria <zlahlassa22dw@ikzubirimanteo.com>
2023-01-06 23:04:48 +0100

    Clase main terminada (fun correctamente)

--- ejercicio5 de los ejercicios3pdf/src/Empresa/main.java
+++ ejercicio5 de los ejercicios3pdf/src/Empresa/main.java
@@ -5,7 +5,7 @@ public class main {
         MobilePhone prueba= new MobilePhone("46789766",90);
         Empleado proba= new Empleado("RAUL","46556TG",190,100,200,prueba);
         proba.nombreypeso();
-        for (int i=0;i<=1;i++){
+        for (int i=0;i<2;i++){
             proba.trabajar();
         }
         proba.nombreypeso();
