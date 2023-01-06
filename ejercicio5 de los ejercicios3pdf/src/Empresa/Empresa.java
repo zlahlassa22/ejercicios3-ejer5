cd1fa9d6a41a0e87e5b4331bc67d443d6ed49b2b
zakaria <zlahlassa22dw@ikzubirimanteo.com>
2023-01-06 22:29:36 +0100

    Clase Empresa terminada

--- ejercicio5 de los ejercicios3pdf/src/Empresa/Empresa.java
+++ ejercicio5 de los ejercicios3pdf/src/Empresa/Empresa.java
@@ -1,5 +1,27 @@
 package Empresa;
 
 public class Empresa {
+     String nombre;
+     int ganancia;
+    Empleado jefe;
+    Empleado gerente;
+    Empresa(String n, int g,Empleado j){
+        nombre=n;
+        ganancia=g;
+        jefe=j;
+    }
+    void contratargerente(Empleado gerente){
+         this.gerente= gerente;
+    }
+    void trabajo(){
+        jefe.trabajar();
+        gerente.trabajar();
+        ganancia=ganancia+100;
+
+    }
+    void mostrar(){
+        System.out.println(nombre+" "+ganancia+" "+jefe+" "+gerente);
+    }
+
 
 }
