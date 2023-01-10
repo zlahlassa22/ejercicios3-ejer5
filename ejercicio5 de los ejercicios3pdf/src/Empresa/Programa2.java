6e482453a1339caacb286c333498c7a726461991
zakaria <zlahlassa22dw@ikzubirimanteo.com>
2023-01-10 16:47:07 +0100

    Clase programa2  terminada

--- ejercicio5 de los ejercicios3pdf/src/Empresa/Programa2.java
+++ ejercicio5 de los ejercicios3pdf/src/Empresa/Programa2.java
@@ -5,7 +5,7 @@ public class Programa2 {
         MobilePhone num= new MobilePhone("12345646",96);
         Empleado jefe= new Empleado("ZAKARIA","564545z",185,80,500,num);
         Empresa emp= new Empresa("ZUBIRI",23506,jefe);
-        Empleado gerente= new Empleado("ZAKA","34546r",183,86,200,num);
+        Empleado gerente= new Empleado("ZAKA","34546ry",183,86,200,num);
         emp.contratargerente(gerente);
         emp.toString();
         System.out.println(emp.toString());
