34de42f8005fa6496c31bc4634acdd5f8f7b64b7
zakaria <zlahlassa22dw@ikzubirimanteo.com>
2023-01-10 16:40:54 +0100

    Clase main terminada

--- ejercicio5 de los ejercicios3pdf/src/Empresa/main.java
+++ ejercicio5 de los ejercicios3pdf/src/Empresa/main.java
@@ -4,11 +4,11 @@ public class main {
     public static void main(String[] args){
         MobilePhone prueba= new MobilePhone("46789766",90);
         Empleado proba= new Empleado("RAUL","46556TG",190,100,200,prueba);
-        proba.nombreypeso();
+        System.out.println(proba.toString());
         for (int i=0;i<2;i++){
             proba.trabajar();
         }
-        proba.nombreypeso();
+        System.out.println(proba.toString());
 
 
 
